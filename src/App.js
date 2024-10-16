fedfcf9e9639383e19d1fb8208a40f5acc15b7f1
WonderSTK <mehulparmar9694@gmail.com>
2024-10-16 17:45:50 +0530

    pushed to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [time, setTime] = useState("");
+  const [countdown, setCountdown] = useState(null);
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+   
+    if (time && !isActive) {
+      setIsActive(true);
+      setCountdown(parseInt(time)); 
+    }
+  };
+
+  useEffect(() => {
+    let timer;
+    if (isActive && countdown > 0) {
+      timer = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000);
+    } else if (countdown === 0) {
+      setIsActive(false);
+      clearInterval(timer);
+    }
+
+    return () => clearInterval(timer); 
+  }, [isActive, countdown]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Timer: {countdown !== null ? countdown : time}</h1>
+      <input
+        type="input"
+        value={time}
+        onChange={(e) => setTime(e.target.value)}
+        placeholder="Enter time in seconds"
+      />
+      <button onClick={handleClick}>Start</button>
     </div>
   );
-}
+};
 
 export default App;
